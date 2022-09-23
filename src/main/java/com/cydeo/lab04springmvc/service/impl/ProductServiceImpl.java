bb4b90b64ecd2c2727a3a70ea45a0f4c1487ae4f
naranburulov <naranburulov@gmail.com>
2022-09-23 12:13:03 -0400

    product search

--- src/main/java/com/cydeo/lab04springmvc/service/impl/ProductServiceImpl.java
+++ src/main/java/com/cydeo/lab04springmvc/service/impl/ProductServiceImpl.java
@@ -8,14 +8,19 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService {
     public static List<Product> PRODUCT_LIST = new ArrayList<>();
     @Override
     public List<Product> searchProduct(String name){
-        // todo implement search structure using string startsWith function
-        return new ArrayList<>();
+        return PRODUCT_LIST
+                .stream()
+                .filter(product -> product.getName().startsWith(name))
+                .collect(Collectors.toList());
+
+
     }
 
     @Override
